4a7edb19457b5b83665e64e303c1a6300a3995d4
themaxtech <maxtechapps@gmail.com>
2015-09-09 14:10:03 +0530

    A15
    A15

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,63 @@
- if (device.platform == 'android' || device.platform == 'Android') {
-pushNotification.register(pushSuccessHandler, pushErrorHandler,{"senderID":"12345678901","ecb":"onNotificationGCM"});
-} else {
-pushNotification.register(pushTokenHandler, pushErrorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});
+//var pushNotification;
+ 
+ // Setup push notifications:
+try
+{
+    var pushNotification = window.plugins.pushNotification;
+    if (window.device.platform == 'iOS') {
+        // Register for IOS:
+        alert("iOS Device");
+        pushNotification.register(
+            pushSuccessHandler,
+            pushErrorHandler, {
+                "badge":"true",
+                "sound":"true",
+                "alert":"true",
+                "ecb":"onNotificationAPNS"
+            }
+        );
+  
+    }
+    else {
+
+    	alert("Device is not an iOS");
+
+    }
+}
+catch(err)
+{
+    // For this example, we'll fail silently ...
+    console.log(err);
+    alert("deviceready error" + err);
+
 }
 
-function pushSuccessHandler(result) {
-alert('result = ' + result);
-}
-function pushErrorHandler(error) {
-alert('error = ' + error);
-}
-function pushTokenHandler(result) {
-alert('iOS device token = ' + result);
-}
+/**
+ * Success handler for when connected to push server
+ * @param result
+ */
+var pushSuccessHandler = function(result)
+{
+    console.log(result);
+    alert("Success" + result);
+};
+ 
+/**
+ * Error handler for when not connected to push server
+ * @param error
+ */
+var pushErrorHandler = function(error)
+{
+    console.log(error);
+    alert("Error" + result);
+};
+ 
+/**
+ * Notification from Apple APNS
+ * @param e
+ */
+var onNotificationAPNS = function(e)
+{
+    // ...
+    alert("Notification Received");
+};
